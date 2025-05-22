ab527d0c6d8a81146e85bf988520915080889ceb
shandli123 <shandlikamal248@gmail.com>
2025-05-22 20:23:39 +0530

    removing time stamp

--- browserstack-report-action/src/main.js
+++ browserstack-report-action/src/main.js
@@ -21,7 +21,6 @@ async function run() {
 
     const initialParams = {
       originalBuildName: buildName,
-      // buildStartedAt: Date.now().toString(), // Changed to timestamp string
       requestingCi: constants.CI_SYSTEM.GITHUB_ACTIONS,
       reportFormat: [constants.REPORT_FORMAT.BASIC_HTML, constants.REPORT_FORMAT.RICH_HTML],
       requestType: constants.REPORT_REQUEST_STATE.FIRST,
